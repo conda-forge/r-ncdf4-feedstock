c2ab9f23df3ed57eacae1b31cc80c32418793b8d
Mervin Fansler <mmfansler@gmail.com>
2024-07-26 01:51:15 +0200

    overwrite makevars

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 set LIBRARY_LIB=%LIBRARY_LIB:\=/%
 set LIBRARY_INC=%LIBRARY_INC:\=/%
 
+REM overwrite Makevars
+echo "PKG_LIBS=-lnetcdf -lhdf5_hl -lhdf5 -lz" > src/Makevars.ucrt
+
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
