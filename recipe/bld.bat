e6a7ecf7dc3c5a12b17ac76d39b7df321c05aece
Mervin Fansler <mmfansler@gmail.com>
2024-07-26 02:06:00 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ set LIBRARY_LIB=%LIBRARY_LIB:\=/%
 set LIBRARY_INC=%LIBRARY_INC:\=/%
 
 REM overwrite Makevars
-echo "PKG_LIBS=-lnetcdf -lhdf5_hl -lhdf5 -lz" > src/Makevars.ucrt
+echo PKG_LIBS = -lnetcdf > src/Makevars.ucrt
 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
